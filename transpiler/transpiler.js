988b8d4eb3a64ee65f16cf86c6344d81d6d3a7bc
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2021-12-24 18:22:43 +0530

    Regular expression for function detection added.
    1) Regular expression for function call
    2)Regular expression for function definition
    3)Regular expression for function declaration

--- transpiler/transpiler.js
+++ transpiler/transpiler.js
@@ -8,11 +8,56 @@ const keywards = {
 //Preprocessor Directives
     //Detect
     //Transpile
+    
 
-    function detectPreprocessor(scanned) {
-        
+    const dataTypeExpression = '(?:\\w+\\**\\s+\\**)+(?:\\s*\\**\\s*)*';
+    const identifierExpression = '(\\w+)\\s*'; 
+    const functionParametersExpression = '(\\(.*\\))';
+    const whiteSpaceExpression = '^\\s*\\t*';
+    //const initializationExpression = ;
+
+
+    const includeDetector = /#include.+/gm;
+    const macroDefinitionDetector = /#define\s(\w+)\s+(.+)/;
+    const functionDefinationDetector = new RegExp(whiteSpaceExpression + dataTypeExpression + identifierExpression + functionParametersExpression + '[^;?]', 'gm');
+    const functionDeclarationDetector = new RegExp(whiteSpaceExpression + dataTypeExpression + identifierExpression + functionParametersExpression + ';', 'gm');
+    const functionCallDetector = new RegExp(whiteSpaceExpression + identifierExpression + functionParametersExpression, 'gm');
+    //const variableDefinitionDetector1 = new RegExp(whiteSpaceExpression + identifierExpression);
+    //const variableDefinitionDetector2 = new RegExp(whiteSpaceExpression + identifierExpression + initializationExpression);
+    let code = 
+    `#include<stdio.h>
+    #include<conio.h>
+    #define X 10
+    #include<stdlib.h>
+        void print (int a);
+        void print (int a, float* d)
+    {
+       printf("%d", a);
+       printf("%d", X);
+    
     }
+    long int x (){
+    
+    }
+    long long x(){
 
+    }
+    int main()
+    {
+       int *a = 7;
+       print(a);
+    }
+    double * fun(){
+    somecode
+    
+    }`;
+    //console.log(code);
+    console.log(whiteSpaceExpression + identifierExpression + functionParametersExpression);
+    console.log(code = code.replace(includeDetector, ''));
+    console.log(code.match(macroDefinitionDetector));
+    console.log(code = code.replace(functionDefinationDetector, 'function $1$2'));
+    console.log(code = code.replace(functionDeclarationDetector, ''));
+    console.log(code.match(functionCallDetector));
 //Function Defination
     //Detect
     //Transpile
