19eafb316acfdceed38a8c76ddd7b8ca067f0894
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2021-12-30 03:27:37 +0530

    Code highlighting functionality added This functionality will enable to select and highlight a certain area of code.

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -2,6 +2,8 @@ import react from 'react';
 import 'codemirror/lib/codemirror.css';
 import 'codemirror/theme/material.css';
 
+import '../customStyle.css'
+
 import { Controlled as ControlledEditor } from 'react-codemirror2';
 import 'codemirror/mode/clike/clike';
 import 'codemirror/mode/javascript/javascript';
@@ -19,6 +21,12 @@ export default function CodeEditor(props) {
     function handleChange(editor, data, value) {
         onChange(value);
     }
+    const highlightLines = (editor, start, end) => {
+        console.log('high');
+        const from = {line: 1, ch: 1};
+        const to = {line: 3, ch: 3};
+        editor.markText(from, to, {className: "codemirror-highlighted"});
+      }
     return (
             <div key={'codewrapper'} style={codewrapper}>
                 
@@ -33,10 +41,19 @@ export default function CodeEditor(props) {
                         lint: true,
                         mode: language,
                         lineNumbers: true,
-                        theme: 'material'
                         
+                         
                     }
                 }
+                
+                selection={{
+                    ranges: [{
+                      anchor: {ch: 1, line: 1},
+                      head: {ch: 3, line: 3}
+                    }],
+                    focus: true // defaults false if not specified
+                  }}
+                  onSelection={(editor, data) => {highlightLines(editor, 0, 0)}}
                 
                 />
             </div>
