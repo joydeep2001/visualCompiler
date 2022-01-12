f27eaadb2e8036db047833ca733009f48135d529
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-01-13 01:38:38 +0530

    functionSignDetector regexp updated

--- client/interpreter/regularExpression.js
+++ client/interpreter/regularExpression.js
@@ -7,9 +7,9 @@ const optionalWhiteSpace = '\\s*\\t*\\n*';
 
 const includeDetector = /#include.+/gm;
 const macroDefinitionDetector = /#define\s(\w+)\s+(.+)/g;
-const functionSignatureDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}(${optionalWhiteSpace})${functionParameters}${optionalWhiteSpace}[^;]`, 'm');
+const functionSignatureDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}(${optionalWhiteSpace})${functionParameters}${optionalWhiteSpace}\{`, 'gm');
 const functionCallDetector = new RegExp(optionalWhiteSpace + identifier + functionParameters, 'gm');
-const variableDefinitionDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}${optionalWhiteSpace}[,=;\)][^\(]`, 'g');
+const variableDefinitionDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}${optionalWhiteSpace}[,=;\)][^\(]`, 'gm');
 // const multiVariableDefinationDetector = new RegExp(`(int)\*?\s((\w+(\[(\d+|\w+)\])?(\s*?=\s*(\d+|'.+|".+"|\w+|\{.\}))?\s*[,;]\s*)+)`);
 const loopDetector = new RegExp(`${optionalWhiteSpace}while|for|do`);
 
