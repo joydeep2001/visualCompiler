8edfccd690045757c8c0bd1c9311c061a5ad71ed
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-01-13 01:41:38 +0530

    createFunctionMap function refactored

--- client/interpreter/utility.js
+++ client/interpreter/utility.js
@@ -55,8 +55,7 @@ function CLikeInterpreterUtilities(code) {
     function extractBody(functionSignEndsAt) {
         
         let startOfBody = functionSignEndsAt;
-        for(;code[startOfBody] != '{';startOfBody++);
-
+        console.log(code[functionSignEndsAt]);
         let braceBalance = 1;
         let endOfBody = startOfBody + 1
         for(;braceBalance !== 0;endOfBody++) {
@@ -94,39 +93,41 @@ function CLikeInterpreterUtilities(code) {
             currentPos += statement.length + 1; //adding one to count the semicolon
         });
     }
-    this.createFunctionMap = () => {
-        let temp = '';
-        let lastFunctionEndIndex = 0;
-        let lineCount = 1;
-        let lastLineEndIndex = 1;
-        let column = 1;
-        let startIndexOfFunction;
-        let functionMap = new Map();
+   
+    this.indexVsLine = [0];
+    this.mapIndexVsLine = () => {
         for(let i = 0;i < code.length;i++) {
-            temp += code[i];
-            column++;
-            if(func = temp.match(functionSignatureDetector)) {
-                startIndexOfFunction = lastFunctionEndIndex + func.index + 1;
-                let startColumn = startIndexOfFunction - lastLineEndIndex;
-                temp = '';
-                lastFunctionEndIndex = i + 1;
-                tokenizeParameters(lineCount, startColumn, func);
-                const {
-                    startOfBody,
-                    endOfBody
-                } = extractBody(i);
-                let funcName = func[4];
-                tokenizeBody(startOfBody, endOfBody, funcName);
-            }
             if(code[i] == '\n') {
-                lineCount++;
-                column = 1; 
-                lastLineEndIndex = i + 1;
-            }         
+                this.indexVsLine.push(i);
+            }
         }
-        if(temp != '') lineCount++;
+        let lastIndex = code.length - 1;
+        if(code[lastIndex] != '\n') this.indexVsLine.push(lastIndex);
+        //console.log(this.indexVsLine);
 
-        //console.log(lineCount);
+    }
+    this.getLineColumn = (index) => {
+        for(let i = 1;i < this.indexVsLine.length;i++) {
+            if(index <= this.indexVsLine[i]) {
+                console.log(i, index - this.indexVsLine[i - 1] + 1);
+                return {
+                    line: i,
+                    column: index - this.indexVsLine[i - 1] + 1
+                }
+                
+            }
+        }
+        console.log("invalid index");
+    }
+
+    this.createFunctionMap = () => {
+        while(func = functionSignatureDetector.exec(code)) {
+            // console.log('starts at ', func.index);
+            console.log('ends at', functionSignatureDetector.lastIndex - 1);
+            //console.log(func[0][func[0].length - 1]);
+            extractBody(functionSignatureDetector.lastIndex - 1);
+        }
+
     }
 
     
@@ -134,4 +135,6 @@ function CLikeInterpreterUtilities(code) {
 
 let inpr = new CLikeInterpreterUtilities(code); 
 //inpr.getMacroMap();
+inpr.mapIndexVsLine();
+
 inpr.createFunctionMap();
