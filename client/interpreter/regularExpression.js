354fdcf46dc8671d9aa795e3f3d7642a55619693
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-01-12 22:51:09 +0530

    regular expression for function detection updated

--- client/interpreter/regularExpression.js
+++ client/interpreter/regularExpression.js
@@ -11,7 +11,7 @@ const functionSignatureDetector = new RegExp(`${dataType}(${optionalWhiteSpace})
 const functionCallDetector = new RegExp(optionalWhiteSpace + identifier + functionParameters, 'gm');
 const variableDefinitionDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}${optionalWhiteSpace}[,=;\)][^\(]`, 'g');
 // const multiVariableDefinationDetector = new RegExp(`(int)\*?\s((\w+(\[(\d+|\w+)\])?(\s*?=\s*(\d+|'.+|".+"|\w+|\{.\}))?\s*[,;]\s*)+)`);
-const loopDetector = new RegExp('while|for|do');
+const loopDetector = new RegExp(`${optionalWhiteSpace}while|for|do`);
 
 console.log(variableDefinitionDetector);
 
