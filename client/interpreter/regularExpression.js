ea98bf72cc8cfead31196432d667df3be3dfdb5d
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-01-22 00:08:24 +0530

    some regular expressions added

--- client/interpreter/regularExpression.js
+++ client/interpreter/regularExpression.js
@@ -1,25 +1,37 @@
 const dataType = '((?:\\w+\\**\\s+\\**)+)(\\s*\\**)*';
 const identifier = '(\\w+)'; 
-const functionParameters = '\\((.*\\n*.*)\\)';
+const parameters = '\\((.*\\n*.*)\\)';
 const optionalWhiteSpace = '\\s*\\t*\\n*';
 
 
 
 const includeDetector = /#include.+/gm;
 const macroDefinitionDetector = /#define\s(\w+)\s+(.+)/g;
-const functionSignatureDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}(${optionalWhiteSpace})${functionParameters}${optionalWhiteSpace}\{`, 'gm');
-const functionCallDetector = new RegExp(optionalWhiteSpace + identifier + functionParameters, 'gm');
-const variableDefinitionDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}${optionalWhiteSpace}[,=;\)][^\(]`, 'gm');
+const functionSignatureDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}(${optionalWhiteSpace})${parameters}${optionalWhiteSpace}\\{`, 'gm');
+const functionCallDetector = new RegExp(optionalWhiteSpace + identifier + parameters, 'gm');
+const parameterDefinitionDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}${optionalWhiteSpace}[,\\)][^\\(]`, 'gm');
 // const multiVariableDefinationDetector = new RegExp(`(int)\*?\s((\w+(\[(\d+|\w+)\])?(\s*?=\s*(\d+|'.+|".+"|\w+|\{.\}))?\s*[,;]\s*)+)`);
-const loopDetector = new RegExp(`${optionalWhiteSpace}while|for|do`);
+const whileloopDetector = new RegExp(`while${optionalWhiteSpace}${parameters}`);
+const forloopDetector = new RegExp(`for${optionalWhiteSpace}${parameters}`);
+const ifDetector = new RegExp(`if${optionalWhiteSpace}${parameters}`);
+const elseIfDetector = new RegExp(`else if${optionalWhiteSpace}${parameters}`);
+const elseDetector = new RegExp(`else if${optionalWhiteSpace}${parameters}`);
 
-console.log(variableDefinitionDetector);
+console.log('functionSignatureDetector', functionSignatureDetector);
+console.log('functionCallDetector', functionCallDetector);
+console.log('parameterDefinitionDetector', parameterDefinitionDetector);
+console.log('whileloopDetector', whileloopDetector);
+console.log('forloopDetector', forloopDetector);
+console.log('if detector', ifDetector);
+console.log('else if detector', elseIfDetector);
+console.log('else detector', elseDetector);
+
 
 module.exports = {
     includeDetector,
     macroDefinitionDetector,
     functionCallDetector,
     functionSignatureDetector,
-    variableDefinitionDetector,
-    loopDetector
+    parameterDefinitionDetector,
+    whileloopDetector
 }
