df97a91fe32af6602acde20d3761da2cb49f132f
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-01-28 13:34:06 +0530

    formatData function added

--- client/src/components/runtime/ActivationRecord.jsx
+++ client/src/components/runtime/ActivationRecord.jsx
@@ -1,12 +1,22 @@
 import "./runtime.css";
-
+function formatData(data) {
+  let formattedData = [];
+  for (let key in data) {
+    formattedData.push({
+      name: key,
+      value: data[key].value,
+    });
+  }
+  console.log(formattedData);
+  return formattedData;
+}
 export default function ActivationRecord(props) {
   return (
     <div className="activationRecords">
       <div className="funcName">{props.functionName}</div>
 
       <div className="variable-cont">
-        {props.data.map((el) => (
+        {formatData(props.data).map(el => (
           <div key={el.name} className="variable">
             <div className="varName">{el.name}</div>
             <div className="varValue">{el.value}</div>
