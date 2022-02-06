471a8bfd7ca55498c68ca731cd5a073cad23b031
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-02-06 19:36:55 +0530

    functionCallDetector regexp updated

--- client/src/interpreter/regularExpression.js
+++ client/src/interpreter/regularExpression.js
@@ -1,15 +1,22 @@
-const dataType = '((?:\\w+\\**\\s+\\**)+)(\\s*\\**)*';
-const identifier = '(\\w+)'; 
-const parameters = '\\((.*\\n*.*)\\)';
-const optionalWhiteSpace = '\\s*\\t*\\n*';
-
-
+const dataType = "((?:\\w+\\**\\s+\\**)+)(\\s*\\**)*";
+const identifier = "(\\w+)";
+const parameters = "\\((.*\\n*.*)\\)";
+const optionalWhiteSpace = "\\s*\\t*\\n*";
 
 const includeDetector = /#include.+/gm;
 const macroDefinitionDetector = /#define\s(\w+)\s+(.+)/g;
-const functionSignatureDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}(${optionalWhiteSpace})${parameters}${optionalWhiteSpace}\\{`, 'gm');
-const functionCallDetector = new RegExp(optionalWhiteSpace + identifier + parameters, 'gm');
-const parameterDefinitionDetector = new RegExp(`${dataType}(${optionalWhiteSpace})${identifier}${optionalWhiteSpace}[,\\)][^\\(]`, 'gm');
+const functionSignatureDetector = new RegExp(
+  `${dataType}(${optionalWhiteSpace})${identifier}(${optionalWhiteSpace})${parameters}${optionalWhiteSpace}\\{`,
+  "gm"
+);
+const functionCallDetector = new RegExp(
+  `${identifier}${optionalWhiteSpace}${parameters}`,
+  ""
+);
+const parameterDefinitionDetector = new RegExp(
+  `${dataType}(${optionalWhiteSpace})${identifier}${optionalWhiteSpace}[,\\)][^\\(]`,
+  "gm"
+);
 // const multiVariableDefinationDetector = new RegExp(`(int)\*?\s((\w+(\[(\d+|\w+)\])?(\s*?=\s*(\d+|'.+|".+"|\w+|\{.\}))?\s*[,;]\s*)+)`);
 const whileloopDetector = new RegExp(`while${optionalWhiteSpace}${parameters}`);
 const partialForLoopDetector = new RegExp(`for${optionalWhiteSpace}\\(.*`);
@@ -17,33 +24,37 @@ const forloopDetector = new RegExp(`for${optionalWhiteSpace}${parameters}`);
 const ifDetector = new RegExp(`if${optionalWhiteSpace}${parameters}`);
 const elseIfDetector = new RegExp(`else if${optionalWhiteSpace}${parameters}`);
 const elseDetector = new RegExp(`else`);
-const variableDetector = new RegExp(`${dataType}${optionalWhiteSpace}${identifier}`);
-const expressionDetector = new RegExp(`(\d*\w*${optionalWhiteSpace}[-/+*=<>]+${optionalWhiteSpace}\d*\w*)*`);
+const variableDetector = new RegExp(
+  `${dataType}${optionalWhiteSpace}${identifier}`
+);
+const expressionDetector = new RegExp(
+  `(\d*\w*${optionalWhiteSpace}[-/+*=<>]+${optionalWhiteSpace}\d*\w*)*`
+);
 
 console.log("-------------debug--------------------");
-console.log('functionSignatureDetector', functionSignatureDetector);
-console.log('functionCallDetector', functionCallDetector);
-console.log('parameterDefinitionDetector', parameterDefinitionDetector);
-console.log('whileloopDetector', whileloopDetector);
-console.log('forloopDetector', forloopDetector);
-console.log('if detector', ifDetector);
-console.log('else if detector', elseIfDetector);
-console.log('else detector', elseDetector);
-console.log('variable detector', variableDetector);
+console.log("functionSignatureDetector", functionSignatureDetector);
+console.log("functionCallDetector", functionCallDetector);
+console.log("parameterDefinitionDetector", parameterDefinitionDetector);
+console.log("whileloopDetector", whileloopDetector);
+console.log("forloopDetector", forloopDetector);
+console.log("if detector", ifDetector);
+console.log("else if detector", elseIfDetector);
+console.log("else detector", elseDetector);
+console.log("variable detector", variableDetector);
 console.log("-------------end--------------------");
 
 module.exports = {
-    includeDetector,
-    macroDefinitionDetector,
-    functionCallDetector,
-    functionSignatureDetector,
-    parameterDefinitionDetector,
-    whileloopDetector,
-    partialForLoopDetector,
-    forloopDetector,
-    ifDetector,
-    elseIfDetector,
-    elseDetector,
-    variableDetector,
-    expressionDetector
-}
+  includeDetector,
+  macroDefinitionDetector,
+  functionCallDetector,
+  functionSignatureDetector,
+  parameterDefinitionDetector,
+  whileloopDetector,
+  partialForLoopDetector,
+  forloopDetector,
+  ifDetector,
+  elseIfDetector,
+  elseDetector,
+  variableDetector,
+  expressionDetector,
+};
