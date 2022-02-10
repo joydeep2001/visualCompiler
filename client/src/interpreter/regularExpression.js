2e276fe38512350271e4aa0fca31e3745d78d979
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-02-10 17:21:28 +0530

    parameter regular expression bug loosely fixed

--- client/src/interpreter/regularExpression.js
+++ client/src/interpreter/regularExpression.js
@@ -1,6 +1,6 @@
 const dataType = "((?:\\w+\\**\\s+\\**)+)(\\s*\\**)*";
 const identifier = "(\\w+)";
-const parameters = "\\((.*\\n*.*)\\)";
+const parameters = "\\((.*)\\)"; //loose fix
 const optionalWhiteSpace = "\\s*\\t*\\n*";
 
 const includeDetector = /#include.+/gm;
@@ -13,20 +13,19 @@ const functionCallDetector = new RegExp(
   `${identifier}${optionalWhiteSpace}${parameters}`,
   ""
 );
-const parameterDefinitionDetector = new RegExp(
-  `${dataType}(${optionalWhiteSpace})${identifier}${optionalWhiteSpace}[,\\)][^\\(]`,
-  "gm"
-);
+
 // const multiVariableDefinationDetector = new RegExp(`(int)\*?\s((\w+(\[(\d+|\w+)\])?(\s*?=\s*(\d+|'.+|".+"|\w+|\{.\}))?\s*[,;]\s*)+)`);
+const variableDetector = new RegExp(
+  `${dataType}${optionalWhiteSpace}${identifier}`
+);
+//const parameters = new RegExp(`\\(${variableDetector}\\)`);
 const whileloopDetector = new RegExp(`while${optionalWhiteSpace}${parameters}`);
 const partialForLoopDetector = new RegExp(`for${optionalWhiteSpace}\\(.*`);
 const forloopDetector = new RegExp(`for${optionalWhiteSpace}${parameters}`);
 const ifDetector = new RegExp(`if${optionalWhiteSpace}${parameters}`);
 const elseIfDetector = new RegExp(`else if${optionalWhiteSpace}${parameters}`);
 const elseDetector = new RegExp(`else`);
-const variableDetector = new RegExp(
-  `${dataType}${optionalWhiteSpace}${identifier}`
-);
+
 const expressionDetector = new RegExp(
   `(\\d*\\w*${optionalWhiteSpace}[-/+*=<>]+${optionalWhiteSpace}\\d*\\w*)+`
 );
@@ -36,7 +35,6 @@ const returnStatementDetector = new RegExp(
 console.log("-------------debug--------------------");
 console.log("functionSignatureDetector", functionSignatureDetector);
 console.log("functionCallDetector", functionCallDetector);
-console.log("parameterDefinitionDetector", parameterDefinitionDetector);
 console.log("whileloopDetector", whileloopDetector);
 console.log("forloopDetector", forloopDetector);
 console.log("if detector", ifDetector);
@@ -52,7 +50,6 @@ module.exports = {
   macroDefinitionDetector,
   functionCallDetector,
   functionSignatureDetector,
-  parameterDefinitionDetector,
   whileloopDetector,
   partialForLoopDetector,
   forloopDetector,
