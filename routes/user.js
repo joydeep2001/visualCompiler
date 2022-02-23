bef3388ca16b1c4a15fd63f753b007785c238207
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-02-23 11:40:54 +0530

    password hashing added

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const _ = require("lodash");
 const { User, validate } = require("../models/user");
 const mongoose = require("mongoose");
@@ -16,6 +17,8 @@ router.post("/", async (req, res) => {
     return res.status(400).send("Username or email already registered..");
 
   user = new User(_.pick(req.body, ["name", "email", "username", "password"]));
+  const salt = await bcrypt.genSalt(10);
+  user.password = await bcrypt.hash(user.password, salt);
   await user.save();
 
   res.status(200).send(_.pick(user, ["name", "email"]));
