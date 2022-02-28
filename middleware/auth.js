466f046f596849e81f40ee6111eab4005f1c1e41
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-02-28 23:57:38 +0530

    jwt is now recieved from cookie of request

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,14 @@ const config = require("config");
 const jwt = require("jsonwebtoken");
 
 function auth(req, res, next) {
-  const token = req.header("x-auth-token");
+  console.log(req.cookies);
+  if (!req.cookies && !req.cookies.authToken) {
+    res.status(400).send("invalid token");
+    return;
+  }
+  const token = req.cookies.authToken;
+  console.log(token);
+
   if (!token) return res.status(401).send("Access Denied. No token found");
 
   try {
@@ -10,7 +17,8 @@ function auth(req, res, next) {
     req.user = decoded;
     next();
   } catch (ex) {
-    res.status(400).redirect("http://localhost:3000/login");
+    console.log(ex);
+    res.status(400).send("invalid token");
   }
 }
 
