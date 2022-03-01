3adf0abdec03fe7162a1214e94d4fe6f0c384a11
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-03-02 00:11:35 +0530

    added some code to fix the homepage of app

--- server.js
+++ server.js
@@ -45,6 +45,11 @@ app.use("/3dmodels", threeDModels);
 
 if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
+  const path = require("path");
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.listen(process.env.PORT);
