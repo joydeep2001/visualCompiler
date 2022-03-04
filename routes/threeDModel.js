3d907509d293882fe49354432652f11c9dca66d5
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-03-04 09:56:35 +0530

    auth middleware temporarily removed from acceesing 3d models

--- routes/threeDModel.js
+++ routes/threeDModel.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 const auth = require("../middleware/auth");
 
-router.post("/", auth, async (req, res) => {
+router.post("/", async (req, res) => {
   try {
     let threeDModels = await ThreeDModel.find();
     console.log(threeDModels);
