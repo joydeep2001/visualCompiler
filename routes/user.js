8f4e281538c60ede4d1b666405b6b67b8db5eb5e
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-03-07 03:30:42 +0530

    one more error message converted into key value

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
+  if (error) return res.status(400).send({ message: error.details[0].message });
   let user = await User.findOne().or([
     { username: req.body.username },
     { email: req.body.email },
