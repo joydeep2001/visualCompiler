5e918839bd245307d9ce4c844517683762e1fc7e
Joydeep Bhattacharjee <2jbhattacharjee@gmail.com>
2022-03-14 02:55:43 +0530

    code formatted

--- client/src/components/CodeEditor.jsx
+++ client/src/components/CodeEditor.jsx
@@ -1,57 +1,48 @@
-import React from 'react';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
+import React from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
 
-import '../customStyle.css'
+import "../customStyle.css";
 
-import { Controlled as ControlledEditor } from 'react-codemirror2';
-import 'codemirror/mode/clike/clike';
-import 'codemirror/mode/javascript/javascript';
+import { Controlled as ControlledEditor } from "react-codemirror2";
+import "codemirror/mode/clike/clike";
+import "codemirror/mode/javascript/javascript";
 
 const codewrapper = {
-    height: '300px',
-   
-}
+  height: "300px",
+};
 
 export default class CodeEditor extends React.Component {
-    
-    handleChange = (editor, data, value) =>{
-        
-        this.props.onChange(value, editor);
-    }
-    
-    render() {
-        return (
-            <div style={codewrapper}>
-                
-                <ControlledEditor 
-                key={'mainEditor'}
-                onBeforeChange={this.handleChange}
-                value={this.props.value}
-                className=''
-                options={
-                    {
-                        lineWrapping: true,
-                        lint: true,
-                        mode: this.props.language,
-                        lineNumbers: true,
-                        theme: 'material'
-                         
-                    }
-                }
-                
-                // selection={{
-                //     ranges: [{
-                //       anchor: {ch: 1, line: 1},
-                //       head: {ch: 3, line: 3}
-                //     }],
-                //     focus: true // defaults false if not specified
-                //   }}
-                  //onSelection={(editor, data) => {this.highlightLines(editor, 1, 1)}}
-                
-                />
-            </div>
-        );
-    }
-    
+  handleChange = (editor, data, value) => {
+    this.props.onChange(value, editor);
+  };
+
+  render() {
+    return (
+      <div style={codewrapper}>
+        <ControlledEditor
+          key={"mainEditor"}
+          onBeforeChange={this.handleChange}
+          value={this.props.value}
+          className=""
+          options={{
+            lineWrapping: true,
+            lint: true,
+            mode: this.props.language,
+            lineNumbers: true,
+            theme: "material",
+          }}
+
+          // selection={{
+          //     ranges: [{
+          //       anchor: {ch: 1, line: 1},
+          //       head: {ch: 3, line: 3}
+          //     }],
+          //     focus: true // defaults false if not specified
+          //   }}
+          //onSelection={(editor, data) => {this.highlightLines(editor, 1, 1)}}
+        />
+      </div>
+    );
+  }
 }
--- client/src/components/OutputBox.jsx
+++ client/src/components/OutputBox.jsx
@@ -1,39 +1,31 @@
-import react from 'react';
-import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/material.css';
+import react from "react";
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/material.css";
 
-import { Controlled as ControlledEditor } from 'react-codemirror2';
+import { Controlled as ControlledEditor } from "react-codemirror2";
 
 const outputwrapper = {
-    height: '200px'
-}
+  height: "200px",
+};
 
 export default function OutputBox(props) {
-    const {
-        value,
-        onChange
-    } = props;
-    function handleChange(editor, data, value) {
-        onChange(value);
-    }
-    return (
-            <div key={'outputwrapper'} style={outputwrapper}>
-                
-                <ControlledEditor 
-                key={'outputbox'}
-                onBeforeChange={handleChange}
-                value={value}
-                
-                className=''
-                options={
-                    {
-                        lineWrapping: true,
-                        theme: 'material',
-                        mode:''
-                    }
-                }
-                
-                />
-            </div>
-    );
+  const { value, onChange } = props;
+  function handleChange(editor, data, value) {
+    onChange(value);
+  }
+  return (
+    <div key={"outputwrapper"} style={outputwrapper}>
+      <ControlledEditor
+        key={"outputbox"}
+        onBeforeChange={handleChange}
+        value={value}
+        className=""
+        options={{
+          lineWrapping: true,
+          theme: "material",
+          mode: "",
+        }}
+      />
+    </div>
+  );
 }
